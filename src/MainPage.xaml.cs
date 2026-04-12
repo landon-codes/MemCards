1350093b4f8e310fac9ee0debeaa2803cf81334c
Landon <230772997+landon-codes@users.noreply.github.com>
2026-04-11 21:55:43 -0500

    Write some of the script for getting sets Does the following: - Checks for a `AppData/Cards` directory. If it does not exist, the app creates one. - Reads the cards in the directory. - Finds the json file for each set and reads it.

--- src/MainPage.xaml.cs
+++ src/MainPage.xaml.cs
@@ -1,9 +1,35 @@
-﻿namespace MemCards;
+﻿using System.IO;
+using MemCardsPackageManager;
+
+namespace MemCards;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
+
+		if (!Directory.Exists("AppData/Cards"))
+		{
+			Directory.CreateDirectory("AppData/Cards");
+		}
+
+		// Add the cards to the main menu
+		// NOTE: If you have a better idea for how to do this, please do!
+		string[] setDirs = Directory.GetDirectories("AppData/Cards");
+		foreach (string set in setDirs)
+		{
+			try
+			{
+				// Sets SHOULD only have **1** json file.
+				// If there are more than one, we're going to ignore the rest
+				string[] jsonFiles = Directory.GetFiles($"AppData/Cards/{set}", "*.json");
+				CardSet importedSet = new CardSet($"AppData/Cards/{set}/{jsonFiles[0]}");
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("A card set failed to load; it may be corrupted or misconfigured.");
+			}
+		} 
 	}
 }
