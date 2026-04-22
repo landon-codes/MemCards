710b403cff13dd1fbcf24c77dab53a24bc03b5df
Landon <230772997+landon-codes@users.noreply.github.com>
2026-04-21 20:57:27 -0500

    Create base of CreateSetPage

---
+++ src/Pages/CreateSetPage.xaml.cs
@@ -0,0 +1,11 @@
+using MemCardsPackageManager;
+
+namespace MemCards;
+
+public partial class CreateSetPage : ContentPage
+{
+	public CreateSetPage()
+    {
+        InitializeComponent();
+    }
+}
