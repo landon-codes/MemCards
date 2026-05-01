33837480f2c2c70ab8a0a1ac33c2a67c8c1877f5
Landon <230772997+landon-codes@users.noreply.github.com>
2026-05-01 09:25:03 -0500

    Replace the old .NET MAUI template with the Avalonia template

--- src/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MemCards;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ src/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MemCards;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
